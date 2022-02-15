dd56ee70c2ffdefb41621c427ea45eec73ac76b4
shenxingchao <864318466@qq.com>
2022-02-15 13:51:30 +0800

    [optimize]1.优化代码结果 创建小工具部件

--- lib/home.dart
+++ lib/home.dart
@@ -97,196 +97,227 @@ class _HomeComponentState extends State<HomeComponent> {
                 child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      //轮播图
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Container(
-                            decoration: const BoxDecoration(boxShadow: [
-                              BoxShadow(
-                                offset: Offset(0, 8), //x,y轴
-                                color: Color(0xffcccccc), //投影颜色
-                                blurRadius: 8, //投影距离
-                              )
-                            ]),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: CarouselSlider(
-                                options: CarouselOptions(
-                                  //宽高比
-                                  aspectRatio: 1400 / 340,
-                                  //可见比例
-                                  viewportFraction: 1,
-                                  //自动播放
-                                  autoPlay: true,
-                                ),
-                                items: bannerList.map((item) {
-                                  return Builder(
-                                    builder: (BuildContext context) {
-                                      return FadeInImage.assetNetwork(
-                                        alignment: Alignment.topRight,
-                                        //图片适应父组件方式  cover:等比缩放水平垂直直到2者都填满父组件 其他的没啥用了
-                                        fit: BoxFit.cover,
-                                        placeholder:
-                                            'assets/images/default_banner.png',
-                                        image: item["pic"] as String,
-                                      );
-                                    },
-                                  );
-                                }).toList(),
-                              ),
-                            )),
-                      ),
-                      //分类图标
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Row(
-                          //相当于css justifly
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          //相当于css align-item
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: categoryList.map((item) {
-                            return Builder(
-                              builder: (BuildContext context) {
-                                return InkWell(
-                                  child: Column(
-                                    children: [
-                                      SizedBox(
-                                        width: 40,
-                                        height: 40,
-                                        child: Image.asset(
-                                          item["icon"],
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
-                                      const SizedBox(height: 5.0),
-                                      Text(item['title'])
-                                    ],
-                                  ),
-                                  onTap: () {
-                                    // Get.toNamed('/book_chapter',
-                                    //     arguments: Book(
-                                    //         id: item["id"],
-                                    //         bookName: item["book_name"]));
-                                  },
-                                );
-                              },
-                            );
-                          }).toList(),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                        child: Container(
-                          alignment: Alignment.centerLeft,
-                          child: const Text(
-                            '歌单推荐',
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Wrap(
-                          //从左到右排列
-                          direction: Axis.horizontal,
-                          //水平间距
-                          spacing: 0,
-                          //垂直间距 此值可以设置为负数 以减小上下之间的间距 不然默认的0有点大
-                          runSpacing: 0,
-                          //相当于水平方向上的 justifly-content
-                          alignment: WrapAlignment.start,
-                          //相当于垂直方向上的 align-item
-                          runAlignment: WrapAlignment.center,
-                          children: playList
-                              .map((item) => FractionallySizedBox(
-                                  widthFactor: 1 / 3,
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(5),
-                                    child: GestureDetector(
-                                      child: Column(
-                                        children: [
-                                          Stack(
-                                            alignment: Alignment.bottomLeft,
-                                            children: [
-                                              AspectRatio(
-                                                  aspectRatio: 1 / 1,
-                                                  child: Container(
-                                                      decoration:
-                                                          const BoxDecoration(
-                                                              boxShadow: [
-                                                            BoxShadow(
-                                                              offset: Offset(
-                                                                  0,
-                                                                  4), //x,y轴
-                                                              color: Color(
-                                                                  0xffcccccc), //投影颜色
-                                                              blurRadius:
-                                                                  4, //投影距离
-                                                            )
-                                                          ]),
-                                                      child: ClipRRect(
-                                                          borderRadius:
-                                                              BorderRadius
-                                                                  .circular(
-                                                                      4),
-                                                          child: FadeInImage
-                                                              .assetNetwork(
-                                                            alignment:
-                                                                Alignment
-                                                                    .topRight,
-                                                            //图片适应父组件方式  cover:等比缩放水平垂直直到2者都填满父组件 其他的没啥用了
-                                                            fit: BoxFit.cover,
-                                                            placeholder:
-                                                                'assets/images/default.png',
-                                                            image:
-                                                                item["img"],
-                                                          )))),
-                                              Padding(
-                                                padding: const EdgeInsets.fromLTRB(5, 0, 0, 5),
-                                                child: Text(
-                                                  (item["listencnt"] / 10000)
-                                                          .toStringAsFixed(2) +
-                                                      "万",
-                                                  style: const TextStyle(
-                                                      color: Colors.white),
-                                                ),
-                                              )
-                                            ],
-                                          ),
-                                          Container(
-                                            margin: const EdgeInsets.fromLTRB(
-                                                0, 5, 0, 5),
-                                            child: Text(
-                                              item["name"],
-                                              maxLines: 1,
-                                              overflow: TextOverflow.ellipsis,
-                                              style: const TextStyle(
-                                                  fontSize: 16),
-                                            ),
-                                          ),
-                                          Text(
-                                            item["uname"],
-                                            maxLines: 1,
-                                            overflow: TextOverflow.ellipsis,
-                                            style: const TextStyle(
-                                                fontSize: 14,
-                                                color: Color(0xff999999)),
-                                          ),
-                                        ],
-                                      ),
-                                      onTap: () {
-                                        // Get.toNamed('/book_chapter',
-                                        //     arguments: Book(
-                                        //         id: item["id"],
-                                        //         bookName: item["book_name"]));
-                                      },
-                                    ),
-                                  )))
-                              .toList(),
-                        ),
-                      )
-                    ])))
+                //轮播图
+                CarouselWidget(bannerList: bannerList),
+                //分类图标
+                CategoryWidget(categoryList: categoryList),
+                //推荐歌单
+                RecommendPlayListWidget(playList: playList)
+              ])))
         : const Loading();
+  }
+}
+
+//推荐歌单
+class RecommendPlayListWidget extends StatelessWidget {
+  const RecommendPlayListWidget({
+    Key? key,
+    required this.playList,
+  }) : super(key: key);
+
+  final List playList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Padding(
+        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+        child: Container(
+          alignment: Alignment.centerLeft,
+          child: const Text(
+            '歌单推荐',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Wrap(
+          //从左到右排列
+          direction: Axis.horizontal,
+          //水平间距
+          spacing: 0,
+          //垂直间距 此值可以设置为负数 以减小上下之间的间距 不然默认的0有点大
+          runSpacing: 0,
+          //相当于水平方向上的 justifly-content
+          alignment: WrapAlignment.start,
+          //相当于垂直方向上的 align-item
+          runAlignment: WrapAlignment.center,
+          children: playList
+              .map((item) => FractionallySizedBox(
+                  widthFactor: 1 / 3,
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: GestureDetector(
+                      child: Column(
+                        children: [
+                          Stack(
+                            alignment: Alignment.bottomLeft,
+                            children: [
+                              AspectRatio(
+                                  aspectRatio: 1 / 1,
+                                  child: Container(
+                                      decoration:
+                                          const BoxDecoration(boxShadow: [
+                                        BoxShadow(
+                                          offset: Offset(0, 4), //x,y轴
+                                          color: Color(0xffcccccc), //投影颜色
+                                          blurRadius: 4, //投影距离
+                                        )
+                                      ]),
+                                      child: ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(4),
+                                          child: FadeInImage.assetNetwork(
+                                            alignment: Alignment.topRight,
+                                            //图片适应父组件方式  cover:等比缩放水平垂直直到2者都填满父组件 其他的没啥用了
+                                            fit: BoxFit.cover,
+                                            placeholder:
+                                                'assets/images/default.png',
+                                            image: item["img"],
+                                          )))),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(5, 0, 0, 5),
+                                child: Text(
+                                  (item["listencnt"] / 10000)
+                                          .toStringAsFixed(2) +
+                                      "万",
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                              )
+                            ],
+                          ),
+                          Container(
+                            margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                            child: Text(
+                              item["name"],
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                          ),
+                          Text(
+                            item["uname"],
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                                fontSize: 14, color: Color(0xff999999)),
+                          ),
+                        ],
+                      ),
+                      onTap: () {
+                        // Get.toNamed('/book_chapter',
+                        //     arguments: Book(
+                        //         id: item["id"],
+                        //         bookName: item["book_name"]));
+                      },
+                    ),
+                  )))
+              .toList(),
+        ),
+      )
+    ]);
+  }
+}
+
+//分类图标
+class CategoryWidget extends StatelessWidget {
+  const CategoryWidget({
+    Key? key,
+    required this.categoryList,
+  }) : super(key: key);
+
+  final List categoryList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        //相当于css justifly
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //相当于css align-item
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: categoryList.map((item) {
+          return Builder(
+            builder: (BuildContext context) {
+              return InkWell(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: 40,
+                      height: 40,
+                      child: Image.asset(
+                        item["icon"],
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    const SizedBox(height: 5.0),
+                    Text(item['title'])
+                  ],
+                ),
+                onTap: () {
+                  // Get.toNamed('/book_chapter',
+                  //     arguments: Book(
+                  //         id: item["id"],
+                  //         bookName: item["book_name"]));
+                },
+              );
+            },
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
+//轮播图
+class CarouselWidget extends StatelessWidget {
+  const CarouselWidget({
+    Key? key,
+    required this.bannerList,
+  }) : super(key: key);
+
+  final List bannerList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+          decoration: const BoxDecoration(boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 8), //x,y轴
+              color: Color(0xffcccccc), //投影颜色
+              blurRadius: 8, //投影距离
+            )
+          ]),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: CarouselSlider(
+              options: CarouselOptions(
+                //宽高比
+                aspectRatio: 1400 / 340,
+                //可见比例
+                viewportFraction: 1,
+                //自动播放
+                autoPlay: true,
+              ),
+              items: bannerList.map((item) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return FadeInImage.assetNetwork(
+                      alignment: Alignment.topRight,
+                      //图片适应父组件方式  cover:等比缩放水平垂直直到2者都填满父组件 其他的没啥用了
+                      fit: BoxFit.cover,
+                      placeholder: 'assets/images/default_banner.png',
+                      image: item["pic"] as String,
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+          )),
+    );
   }
 }
--- lib/tabbar.dart
+++ lib/tabbar.dart
@@ -54,120 +54,137 @@ class _TabbarComponentState extends State<TabbarComponent> {
       overlayColor: const Color(0xff000000),
       //主屏幕迷糊量
       overlayBlur: 2,
-      menuScreen: Container(
-        width: MediaQuery.of(context).size.width * (0.8),
-        height: MediaQuery.of(context).size.height,
-        color: Colors.white,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Image.asset(
-              'assets/images/drawer.png',
-              fit: BoxFit.fitWidth,
-            ),
-            Expanded(
-              child: MediaQuery.removePadding(
-                context: context,
-                removeTop: true,
-                child: Scaffold(
-                  body: ListView(
-                    //垂直列表 水平列表有滚动条哦
-                    scrollDirection: Axis.vertical,
-                    children: [
-                      ListTile(
-                        leading: Icon(
-                          Icons.history,
-                          color: Theme.of(context).colorScheme.primary,
-                        ),
-                        title: const Text('更换主题色'),
-                        tileColor: Colors.white,
-                        onTap: () {
-                          Get.toNamed(
-                            '/theme',
-                          );
-                        },
-                      ),
-                    ],
+      menuScreen: const MenuScreenWidget(),
+      //主屏幕
+      mainScreen: getMainScreen(context),
+    );
+  }
+
+  //主屏幕
+  Scaffold getMainScreen(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarComponent(
+        currentIndex == 0
+            ? InputComponent(
+                height: 40,
+                hasBorder: false,
+                isCircle: true,
+                showSearchIcon: true,
+                placeholder: "歌曲/歌手/歌单/MV",
+                onSubmitted: (value) {
+                  // Get.toNamed('/search_list', arguments: value);
+                })
+            : Text(appBarTitle[currentIndex]),
+        leading: GestureDetector(
+            child: const Icon(Icons.menu),
+            onTap: () {
+              zoomDrawerController.toggle?.call();
+            }),
+        appBarHeight: currentIndex == 0 ? 70 : 66.0,
+        elevation: 0,
+        shadowColor: Colors.transparent,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: router[currentIndex],
+      //tabbar
+      bottomNavigationBar: BottomNavigationBar(
+        //图标大小
+        iconSize: 26,
+        //当前激活项
+        currentIndex: currentIndex,
+        //布局类型
+        type: BottomNavigationBarType.fixed,
+        //选中字体
+        selectedFontSize: 14,
+        selectedItemColor: const Color(0xff333333),
+        //没选中字体
+        unselectedFontSize: 14,
+        //背景颜色
+        backgroundColor: Colors.white,
+        //子节点
+        items: [
+          const BottomNavigationBarItem(label: '主页', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(
+              label: '',
+              icon: Stack(
+                alignment: Alignment.bottomCenter,
+                clipBehavior: Clip.none,
+                children: [
+                  const SizedBox(
+                    width: 0,
+                    height: 0,
                   ),
+                  Positioned(
+                      top: -15,
+                      child: Image.asset('assets/images/icons/music.png',
+                          fit: BoxFit.cover))
+                ],
+              )),
+          const BottomNavigationBarItem(
+              label: '我的', icon: Icon(Icons.person)),
+        ],
+        //切换事件
+        onTap: (int index) {
+          setState(() {
+            if (index != 1) {
+              //点击音乐图标直接进入音乐详情
+              currentIndex = index;
+            }
+          });
+        },
+      ),
+    );
+  }
+}
+
+//侧边栏屏幕
+class MenuScreenWidget extends StatelessWidget {
+  const MenuScreenWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * (0.8),
+      height: MediaQuery.of(context).size.height,
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Image.asset(
+            'assets/images/drawer.png',
+            fit: BoxFit.fitWidth,
+          ),
+          Expanded(
+            child: MediaQuery.removePadding(
+              context: context,
+              removeTop: true,
+              child: Scaffold(
+                body: ListView(
+                  //垂直列表 水平列表有滚动条哦
+                  scrollDirection: Axis.vertical,
+                  children: [
+                    ListTile(
+                      leading: Icon(
+                        Icons.history,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                      title: const Text('更换主题色'),
+                      tileColor: Colors.white,
+                      onTap: () {
+                        Get.toNamed(
+                          '/theme',
+                        );
+                      },
+                    ),
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
-      ),
-      //主屏幕
-      mainScreen: Scaffold(
-        appBar: AppBarComponent(
-          currentIndex == 0
-              ? InputComponent(
-                  height: 40,
-                  hasBorder: false,
-                  isCircle: true,
-                  showSearchIcon: true,
-                  placeholder: "歌曲/歌手/歌单/MV",
-                  onSubmitted: (value) {
-                    // Get.toNamed('/search_list', arguments: value);
-                  })
-              : Text(appBarTitle[currentIndex]),
-          leading: GestureDetector(
-              child: const Icon(Icons.menu),
-              onTap: () {
-                zoomDrawerController.toggle?.call();
-              }),
-          appBarHeight: currentIndex == 0 ? 70 : 66.0,
-          elevation: 0,
-          shadowColor: Colors.transparent,
-          backgroundColor: Theme.of(context).colorScheme.primary,
-        ),
-        body: router[currentIndex],
-        //tabbar
-        bottomNavigationBar: BottomNavigationBar(
-          //图标大小
-          iconSize: 26,
-          //当前激活项
-          currentIndex: currentIndex,
-          //布局类型
-          type: BottomNavigationBarType.fixed,
-          //选中字体
-          selectedFontSize: 14,
-          selectedItemColor: const Color(0xff333333),
-          //没选中字体
-          unselectedFontSize: 14,
-          //背景颜色
-          backgroundColor: Colors.white,
-          //子节点
-          items: [
-            const BottomNavigationBarItem(label: '主页', icon: Icon(Icons.home)),
-            BottomNavigationBarItem(
-                label: '',
-                icon: Stack(
-                  alignment: Alignment.bottomCenter,
-                  clipBehavior: Clip.none,
-                  children: [
-                    const SizedBox(
-                      width: 0,
-                      height: 0,
-                    ),
-                    Positioned(
-                        top: -15,
-                        child: Image.asset('assets/images/icons/music.png',
-                            fit: BoxFit.cover))
-                  ],
-                )),
-            const BottomNavigationBarItem(
-                label: '我的', icon: Icon(Icons.person)),
-          ],
-          //切换事件
-          onTap: (int index) {
-            setState(() {
-              if (index != 1) {
-                //点击音乐图标直接进入音乐详情
-                currentIndex = index;
-              }
-            });
-          },
-        ),
+          ),
+        ],
       ),
     );
   }
