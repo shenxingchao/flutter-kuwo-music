83c9e140343727b1bbcea1f8f04adc6d68124ca8
shenxingchao <864318466@qq.com>
2022-02-18 17:18:03 +0800

    [add]1.首页tabbar歌曲播放旋转动画

--- lib/tabbar.dart
+++ lib/tabbar.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 import 'package:get/get.dart';
@@ -14,7 +15,8 @@ class TabbarComponent extends StatefulWidget {
   _TabbarComponentState createState() => _TabbarComponentState();
 }
 
-class _TabbarComponentState extends State<TabbarComponent> {
+class _TabbarComponentState extends State<TabbarComponent>
+    with SingleTickerProviderStateMixin {
   //当前激活路由索引
   int currentIndex = 0;
   //tabbar路由列表
@@ -28,6 +30,29 @@ class _TabbarComponentState extends State<TabbarComponent> {
 
   //侧边栏控制器
   final zoomDrawerController = ZoomDrawerController();
+
+  //定义动画控制器
+  late AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    initAnimation();
+  }
+
+  @override
+  void dispose() {
+    //路由销毁时需要释放动画资源
+    animationController.dispose();
+    super.dispose();
+  }
+
+  //初始化旋转动画
+  void initAnimation() {
+    //初始化动画控制器
+    animationController =
+        AnimationController(duration: const Duration(seconds: 6), vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,84 +81,141 @@ class _TabbarComponentState extends State<TabbarComponent> {
       overlayBlur: 2,
       menuScreen: const MenuScreenWidget(),
       //主屏幕
-      mainScreen: getMainScreen(context),
+      mainScreen: getMainScreen(context,animationController),
     );
   }
 
   //主屏幕
-  Scaffold getMainScreen(BuildContext context) {
+  Scaffold getMainScreen(BuildContext context,animationController) {
     return Scaffold(
-      appBar: AppBarComponent(
-        currentIndex == 0
-            ? InputComponent(
-                height: 40,
-                hasBorder: false,
-                isCircle: true,
-                showSearchIcon: true,
-                placeholder: "歌曲/歌手/歌单/MV",
-                onSubmitted: (value) {
-                  // Get.toNamed('/search_list', arguments: value);
-                })
-            : Text(appBarTitle[currentIndex]),
-        leading: GestureDetector(
-            child: const Icon(Icons.menu),
-            onTap: () {
-              zoomDrawerController.toggle?.call();
-            }),
-        appBarHeight: currentIndex == 0 ? 70 : 66.0,
-        elevation: 0,
-        shadowColor: Colors.transparent,
-        backgroundColor: Theme.of(context).colorScheme.primary,
-      ),
-      body: router[currentIndex],
-      //tabbar
-      bottomNavigationBar: BottomNavigationBar(
-        //图标大小
-        iconSize: 26,
-        //当前激活项
-        currentIndex: currentIndex,
-        //布局类型
-        type: BottomNavigationBarType.fixed,
-        //选中字体
-        selectedFontSize: 14,
-        selectedItemColor: const Color(0xff333333),
-        //没选中字体
-        unselectedFontSize: 14,
-        //背景颜色
-        backgroundColor: Colors.white,
-        //子节点
-        items: [
-          const BottomNavigationBarItem(label: '主页', icon: Icon(Icons.home)),
-          BottomNavigationBarItem(
-              label: '',
-              icon: Stack(
-                alignment: Alignment.bottomCenter,
-                clipBehavior: Clip.none,
-                children: [
-                  const SizedBox(
-                    width: 0,
-                    height: 0,
-                  ),
-                  Positioned(
-                      top: -15,
-                      child: Image.asset('assets/images/icons/music.png',
-                          fit: BoxFit.cover))
+        appBar: AppBarComponent(
+          currentIndex == 0
+              ? InputComponent(
+                  height: 40,
+                  hasBorder: false,
+                  isCircle: true,
+                  showSearchIcon: true,
+                  placeholder: "歌曲/歌手/歌单/MV",
+                  onSubmitted: (value) {
+                    // Get.toNamed('/search_list', arguments: value);
+                  })
+              : Text(appBarTitle[currentIndex]),
+          leading: GestureDetector(
+              child: const Icon(Icons.menu),
+              onTap: () {
+                zoomDrawerController.toggle?.call();
+              }),
+          appBarHeight: currentIndex == 0 ? 70 : 66.0,
+          elevation: 0,
+          shadowColor: Colors.transparent,
+          backgroundColor: Theme.of(context).colorScheme.primary,
+        ),
+        body: router[currentIndex],
+        //tabbar
+        bottomNavigationBar: GetBuilder<Store>(
+            //初始化store控制器
+            init: Store(),
+            builder: (store) {
+              audioListen(store);
+              return BottomNavigationBar(
+                //图标大小
+                iconSize: 26,
+                //当前激活项
+                currentIndex: currentIndex,
+                //布局类型
+                type: BottomNavigationBarType.fixed,
+                //选中字体
+                selectedFontSize: 14,
+                selectedItemColor: const Color(0xff333333),
+                //没选中字体
+                unselectedFontSize: 14,
+                //背景颜色
+                backgroundColor: Colors.white,
+                //子节点
+                items: [
+                  const BottomNavigationBarItem(
+                      label: '主页', icon: Icon(Icons.home)),
+                  BottomNavigationBarItem(
+                      label: '',
+                      icon: Stack(
+                        alignment: Alignment.bottomCenter,
+                        clipBehavior: Clip.none,
+                        children: [
+                          const SizedBox(
+                            width: 0,
+                            height: 0,
+                          ),
+                          Positioned(
+                            top: -15,
+                            child: RotationTransition(
+                              turns: animationController
+                                ..addStatusListener((status) {
+                                  if (store.audioPlayState ==
+                                          PlayerState.PLAYING &&
+                                      status == AnimationStatus.completed) {
+                                    animationController.reset();
+                                    animationController.forward();
+                                  }
+                                }),
+                              //设置动画的旋转中心
+                              alignment: Alignment.center,
+                              child: ClipOval(
+                                  child: store.playMusicInfo != null &&
+                                          store.playMusicInfo!.pic120 != ''
+                                      ? Image.network(
+                                          store.playMusicInfo!.pic120,
+                                          alignment: Alignment.center,
+                                          //图片适应父组件方式  cover:等比缩放水平垂直直到2者都填满父组件 其他的没啥用了
+                                          fit: BoxFit.cover,
+                                          width: 50,
+                                          height: 50,
+                                          errorBuilder: (BuildContext context,
+                                              Object exception,
+                                              StackTrace? stackTrace) {
+                                            return Image.asset(
+                                              'assets/images/default.png',
+                                              fit: BoxFit.cover,
+                                              width: 50,
+                                              height: 50,
+                                            );
+                                          },
+                                        )
+                                      : Image.asset(
+                                          'assets/images/icons/music.png',
+                                          fit: BoxFit.cover,
+                                          width: 50,
+                                          height: 50,
+                                        )),
+                            ),
+                          )
+                        ],
+                      )),
+                  const BottomNavigationBarItem(
+                      label: '我的', icon: Icon(Icons.person)),
                 ],
-              )),
-          const BottomNavigationBarItem(
-              label: '我的', icon: Icon(Icons.person)),
-        ],
-        //切换事件
-        onTap: (int index) {
-          setState(() {
-            if (index != 1) {
-              //点击音乐图标直接进入音乐详情
-              currentIndex = index;
-            }
-          });
-        },
-      ),
-    );
+                //切换事件
+                onTap: (int index) {
+                  setState(() {
+                    if (index != 1) {
+                      //点击音乐图标直接进入音乐详情
+                      currentIndex = index;
+                    }
+                  });
+                },
+              );
+            }));
+  }
+
+  //监听播放状态改变旋转动画
+  void audioListen(Store store) {
+    if (store.audioPlayState == PlayerState.PLAYING) {
+      animationController.forward();
+    }
+    if (store.audioPlayState == PlayerState.PAUSED ||
+        store.audioPlayState == PlayerState.STOPPED ||
+        store.audioPlayState == PlayerState.COMPLETED) {
+      animationController.stop();
+    }
   }
 }
 
