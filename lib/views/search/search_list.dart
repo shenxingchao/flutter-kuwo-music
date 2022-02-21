2adf9366a743735ddc0037065d1e6c4438f0317b
shenxingchao <864318466@qq.com>
2022-02-21 10:40:02 +0800

    [add]1.完成搜搜-歌曲列表

--- lib/views/search/search_list.dart
+++ lib/views/search/search_list.dart
@@ -8,6 +8,7 @@ import '../../component/loading.dart';
 import '../../interface/play_list_music.dart';
 import '../../store/store.dart';
 import '../../utils/request.dart';
+import '../common/bottom_bar.dart';
 import '../common/music_list.dart';
 
 class SearchListComponent extends StatefulWidget {
@@ -35,20 +36,30 @@ class _SearchListComponentState extends State<SearchListComponent>
   ];
   //tab激活项
   int tabItemIndex = 0;
-
-  //列表
-  List list = [];
+  //请求的url列表
+  List<String> urlList = [
+    'search/getSearchMusicList',
+    'search/getSearchAlbumList',
+    'search/getSearchMVList',
+    'search/getSearchPlayList',
+    'search/getSearchArtistList',
+  ];
+  //5个列表
+  List<List> list = [[], [], [], [], []];
   //当前页
-  int page = 1;
+  List<int> pages = [1, 1, 1, 1, 1];
   //分页数
   final int pageSize = 20;
-  //初次进入页面显示加载动画 后续显示下拉刷新动画
-  bool showLoading = true;
   //列表是否全部加载完成
-  bool loadFinished = false;
+  List<bool> loadFinisheds = [false, false, false, false, false];
   //定义刷新控件 多个
-  final RefreshController refreshController =
-      RefreshController(initialRefresh: false);
+  final List<RefreshController> refreshControllers = [
+    RefreshController(initialRefresh: false),
+    RefreshController(initialRefresh: false),
+    RefreshController(initialRefresh: false),
+    RefreshController(initialRefresh: false),
+    RefreshController(initialRefresh: false),
+  ];
 
   @override
   void initState() {
@@ -60,13 +71,15 @@ class _SearchListComponentState extends State<SearchListComponent>
       ..addListener(() {
         setState(() {
           tabItemIndex = tabController.index;
-          onRefresh();
+          if (list[tabItemIndex].isEmpty) {
+            onRefresh();
+          }
         });
       });
 
     //初始化文本控制器
     textController.text = keyword;
-    //下拉刷新获取数据
+    //下拉刷新获取第一个tabview数据
     onRefresh();
   }
 
@@ -74,7 +87,10 @@ class _SearchListComponentState extends State<SearchListComponent>
   void dispose() {
     //释放controller
     tabController.dispose();
-    refreshController.dispose();
+    //释放refreshControllers
+    for (var element in refreshControllers) {
+      element.dispose();
+    }
     super.dispose();
   }
 
@@ -83,61 +99,44 @@ class _SearchListComponentState extends State<SearchListComponent>
     if (mounted) {
       setState(() {
         //重置加载状态
-        loadFinished = false;
-        page = 1;
-        list.clear();
-        refreshController.loadComplete();
+        loadFinisheds[tabItemIndex] = false;
+        pages[tabItemIndex] = 1;
+        list[tabItemIndex].clear();
+        refreshControllers[tabItemIndex].loadComplete();
       });
       //获取数据
       getData();
       //下拉刷新完成
-      refreshController.refreshCompleted();
-      //首次渲染完成
-      showLoading = false;
+      refreshControllers[tabItemIndex].refreshCompleted();
     }
   }
 
-//上拉加载方法
+  //上拉加载方法
   void onLoading() async {
     //模拟请求完成
     if (mounted) {
       setState(() {
-        page++;
+        pages[tabItemIndex]++;
       });
       //获取数据
-      getData();
-      if (loadFinished) {
+      await getData();
+      if (loadFinisheds[tabItemIndex]) {
         //数据加载完毕
-        refreshController.loadNoData();
+        refreshControllers[tabItemIndex].loadNoData();
       } else {
         //下拉加载完成
-        refreshController.loadComplete();
+        refreshControllers[tabItemIndex].loadComplete();
       }
     }
   }
 
   //获取数据统一方法
-  void getData() async {
-    switch (tabItemIndex) {
-      case 0:
-        //获取搜索列表-歌曲
-        await getSearchMusicList();
-        break;
-      case 1:
-        //获取搜索列表-专辑
-        await getSearchAlbumList();
-        break;
-      default:
-        break;
-    }
-  }
-
-  //获取搜索列表-歌曲
-  Future getSearchMusicList() async {
+  Future getData() async {
     var res = await Request.http(
-        url: 'search/getSearchMusicList',
-        type: 'get',
-        data: {"key": keyword, "pn": page, "rn": pageSize}).then((res) {
+            url: urlList[tabItemIndex],
+            type: 'get',
+            data: {"key": keyword, "pn": pages[tabItemIndex], "rn": pageSize})
+        .then((res) {
       if (res.data["code"] != 200) {
         Fluttertoast.showToast(
           msg: res.data["msg"],
@@ -150,40 +149,29 @@ class _SearchListComponentState extends State<SearchListComponent>
       );
     });
     if (mounted && res != null) {
+      var data = [];
       setState(() {
-        if (res.data["data"]["list"].length == 0) {
-          loadFinished = true;
-        } else {
-          list.addAll(res.data["data"]["list"]);
+        switch (tabItemIndex) {
+          case 0:
+            data = res.data["data"]["list"];
+            break;
+          case 1:
+            data = res.data["data"]["albumList"];
+            break;
+          case 2:
+            data = res.data["data"]["mvlist"];
+            break;
+          case 3:
+            data = res.data["data"]["list"];
+            break;
+          case 4:
+            data = res.data["data"]["list"];
+            break;
         }
-      });
-    }
-    return res;
-  }
-
-  //获取搜索列表-歌曲
-  Future getSearchAlbumList() async {
-    var res = await Request.http(
-        url: 'search/getSearchAlbumList',
-        type: 'get',
-        data: {"key": keyword, "pn": page, "rn": pageSize}).then((res) {
-      if (res.data["code"] != 200) {
-        Fluttertoast.showToast(
-          msg: res.data["msg"],
-        );
-      }
-      return res;
-    }).catchError((error) {
-      Fluttertoast.showToast(
-        msg: "请求服务器错误",
-      );
-    });
-    if (mounted && res != null) {
-      setState(() {
-        if (res.data["data"]["albumList"].length == 0) {
-          loadFinished = true;
+        if (data.isEmpty) {
+          loadFinisheds[tabItemIndex] = true;
         } else {
-          list.addAll(res.data["data"]["albumList"]);
+          list[tabItemIndex].addAll(data);
         }
       });
     }
@@ -202,6 +190,9 @@ class _SearchListComponentState extends State<SearchListComponent>
                 showSearchIcon: true,
                 placeholder: "歌曲/歌手/歌单/MV",
                 onSubmitted: (value) {
+                  setState(() {
+                    keyword = value;
+                  });
                   onRefresh();
                 }),
             appBarHeight: 120.0,
@@ -221,125 +212,153 @@ class _SearchListComponentState extends State<SearchListComponent>
                     tabs: tabItemList
                         .map((tabItem) => Tab(text: tabItem))
                         .toList(),
-                    onTap: (e) {
-                      setState(() {
-                        tabItemIndex = e;
-                        onRefresh();
-                      });
-                    },
                   ),
                 ))),
-        body: list.isNotEmpty
+        body: list[tabItemIndex].isNotEmpty
             ? GetBuilder<Store>(
                 //初始化store控制器
                 init: Store(),
                 builder: (store) {
-                  return TabBarView(
-                    //构建
-                    controller: tabController,
-                    children: tabItemList.map((e) {
-                      return Column(children: [
-                        Offstage(
-                          offstage: e != '单曲',
-                          child: Container(
-                            height: 50,
-                            color: Colors.white,
-                            child: Container(
-                                height: 49.5,
-                                //定义样式
-                                decoration: const BoxDecoration(
-                                  //边框
-                                  border: Border(
-                                    bottom: BorderSide(
-                                      width: 0.5, //宽度
-                                      color: Color(0xffcccccc), //边框颜色
-                                    ),
+                  return Column(children: [
+                    Expanded(
+                      flex: 1,
+                      child: TabBarView(
+                        //构建
+                        controller: tabController,
+                        children: tabItemList.asMap().entries.map((entry) {
+                          var item = entry.value;
+                          var index = entry.key;
+                          return Column(children: [
+                            PlayMusicListWidget(item: item, list: list),
+                            Expanded(
+                              flex: 1,
+                              child: SmartRefresher(
+                                  //下拉刷新
+                                  enablePullDown: true,
+                                  //上拉加载
+                                  enablePullUp: true,
+                                  //经典header 其他[ClassicHeader],[WaterDropMaterialHeader],[MaterialClassicHeader],[WaterDropHeader],[BezierCircleHeader]
+                                  header: const ClassicHeader(
+                                    releaseText: "松开刷新",
+                                    refreshingText: '刷新中...',
+                                    completeText: '刷新完成',
+                                    idleText: '下拉刷新',
                                   ),
-                                ),
-                                child: GestureDetector(
-                                  //保证空白范围可点击 这里点击一行
-                                  behavior: HitTestBehavior.opaque,
-                                  child: Row(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: [
-                                      Row(children: [
-                                        Container(
-                                          margin: const EdgeInsets.fromLTRB(
-                                              10, 0, 5, 0),
-                                          child: const Icon(
-                                              Icons.play_circle_outline,
-                                              color: Color(0xff999999)),
-                                        ),
-                                        Text('播放全部(' +
-                                            list.length.toString() +
-                                            ')首'),
-                                      ]),
-                                    ],
+                                  footer: const ClassicFooter(
+                                    canLoadingText: '松开加载',
+                                    loadingText: '加载中...',
+                                    idleText: '上拉加载',
+                                    noDataText: '没有更多了^_^',
                                   ),
-                                  onTap: () {
-                                    List<PlayListMusic> audioList = [];
-
-                                    for (var element in list) {
-                                      audioList.add(PlayListMusic(
-                                          artist: element["artist"],
-                                          rid: element["rid"],
-                                          name: element["name"],
-                                          isLocal: false,
-                                          pic120: element["pic120"]));
-                                    }
-
-                                    store.playAudioList(audioList);
-                                  },
-                                )),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: SmartRefresher(
-                              //下拉刷新
-                              enablePullDown: true,
-                              //上拉加载
-                              enablePullUp: true,
-                              //经典header 其他[ClassicHeader],[WaterDropMaterialHeader],[MaterialClassicHeader],[WaterDropHeader],[BezierCircleHeader]
-                              header: const ClassicHeader(
-                                releaseText: "松开刷新",
-                                refreshingText: '刷新中...',
-                                completeText: '刷新完成',
-                                idleText: '下拉刷新',
-                              ),
-                              footer: const ClassicFooter(
-                                canLoadingText: '松开加载',
-                                loadingText: '加载中...',
-                                idleText: '上拉加载',
-                                noDataText: '没有更多了^_^',
-                              ),
-                              controller: refreshController,
-                              onRefresh: onRefresh,
-                              onLoading: onLoading,
-                              child: CustomScrollView(slivers: <Widget>[
-                                Builder(builder: (context) {
-                                  if (e == '单曲') {
-                                    return ListWidget(list: list);
-                                  } else {
-                                    return SliverList(
-                                        delegate: SliverChildListDelegate([
-                                      Column(children: [
-                                        ...list
-                                            .asMap()
-                                            .entries
-                                            .map((entry) => Text('3333'))
-                                            .toList()
-                                      ])
-                                    ]));
-                                  }
-                                })
-                              ])),
-                        ),
-                      ]);
-                    }).toList(),
-                  );
+                                  controller: refreshControllers[index],
+                                  onRefresh: onRefresh,
+                                  onLoading: onLoading,
+                                  child: CustomScrollView(slivers: <Widget>[
+                                    Builder(
+                                      builder: (context) {
+                                        if (item == '单曲') {
+                                          return ListWidget(list: list[0]);
+                                        } else if (item == '专辑') {
+                                          return SliverList(
+                                              delegate:
+                                                  SliverChildListDelegate([
+                                            Column(children: [
+                                              ...list[1]
+                                                  .asMap()
+                                                  .entries
+                                                  .map((entry) =>
+                                                      const Text('3333'))
+                                                  .toList()
+                                            ])
+                                          ]));
+                                        } else {
+                                          return SliverList(
+                                              delegate:
+                                                  SliverChildListDelegate([
+                                            Column(
+                                                children: const [Text('未完成')])
+                                          ]));
+                                        }
+                                      },
+                                    )
+                                  ])),
+                            )
+                          ]);
+                        }).toList(),
+                      ),
+                    ),
+                    const PlayMusicBottomBar()
+                  ]);
                 })
             : const Loading());
+  }
+}
+
+//播放全部工具栏
+class PlayMusicListWidget extends StatelessWidget {
+  const PlayMusicListWidget({
+    Key? key,
+    required this.item,
+    required this.list,
+  }) : super(key: key);
+
+  final dynamic item;
+  final List<List> list;
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<Store>(
+        //初始化store控制器
+        init: Store(),
+        builder: (store) {
+          return Offstage(
+            offstage: item != '单曲',
+            child: Container(
+              height: 50,
+              color: Colors.white,
+              child: Container(
+                  height: 49.5,
+                  //定义样式
+                  decoration: const BoxDecoration(
+                    //边框
+                    border: Border(
+                      bottom: BorderSide(
+                        width: 0.5, //宽度
+                        color: Color(0xffcccccc), //边框颜色
+                      ),
+                    ),
+                  ),
+                  child: GestureDetector(
+                    //保证空白范围可点击 这里点击一行
+                    behavior: HitTestBehavior.opaque,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(children: [
+                          Container(
+                            margin: const EdgeInsets.fromLTRB(10, 0, 5, 0),
+                            child: const Icon(Icons.play_circle_outline,
+                                color: Color(0xff999999)),
+                          ),
+                          Text('播放全部(' + list[0].length.toString() + ')首'),
+                        ]),
+                      ],
+                    ),
+                    onTap: () {
+                      List<PlayListMusic> audioList = [];
+                      for (var element in list[0]) {
+                        audioList.add(PlayListMusic(
+                            artist: element["artist"],
+                            rid: element["rid"],
+                            name: element["name"],
+                            isLocal: false,
+                            pic120: element["pic120"]));
+                      }
+                      store.playAudioList(audioList);
+                    },
+                  )),
+            ),
+          );
+        });
   }
 }
