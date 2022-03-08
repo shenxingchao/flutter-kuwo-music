dc0d98394184c4724edc4fbe38127d45ad4372c9
shenxingchao <864318466@qq.com>
2022-03-08 17:28:55 +0800

    [bug fix]1.修复sqlite数据库表结构错误

--- lib/utils/db.dart
+++ lib/utils/db.dart
@@ -33,28 +33,28 @@ class Db {
     //歌单名称表
     await db.execute('''CREATE TABLE custom_play_list (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT comment '歌单名称'
+            name TEXT comment -- 歌单名称
         )''');
     //自定义歌单列表表格
     await db.execute('''CREATE TABLE custom_play_list_content (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        custom_play_list_id INTEGER comment '歌单id',
-        artist TEXT comment '歌手名称',
-        pic TEXT comment '歌曲封面图',
-        rid INTEGER comment '歌曲id',
-        duration INTEGER comment '歌曲时长(s)',
-        mvPlayCnt INTEGER comment 'MV播放次数',
-        hasLossless INTEGER comment '无损音质 1 true 0 false',
-        hasmv INTEGER comment '是否有MV(1有0没有)',
-        releaseDate TEXT comment '发布日期',
-        album TEXT comment '专辑名称',
-        albumid INTEGER comment '专辑id',
-        artistid INTEGER comment '歌手id',
-        songTimeMinutes TEXT comment '歌曲时长格式化(ii:ss)',
-        isListenFee INTEGER comment '是否付费(1 true付费 0false免费)',
-        pic120 TEXT comment '歌曲封面120px缩略图',
-        albuminfo TEXT comment '专辑描述',
-        name TEXT comment '歌曲名称',
+        custom_play_list_id INTEGER,-- 歌单id
+        artist TEXT,-- 歌手名称
+        pic TEXT,-- 歌曲封面图
+        rid INTEGER,-- 歌曲id
+        duration INTEGER,-- 歌曲时长(s)
+        mvPlayCnt INTEGER,-- MV播放次数
+        hasLossless INTEGER,-- 无损音质 1 true 0 false
+        hasmv INTEGER,-- 是否有MV(1有0没有)
+        releaseDate TEXT,-- 发布日期
+        album TEXT,-- 专辑名称
+        albumid INTEGER,-- 专辑id
+        artistid INTEGER,-- 歌手id
+        songTimeMinutes TEXT,-- 歌曲时长格式化(ii:ss)
+        isListenFee INTEGER,-- 是否付费(1 true付费 0false免费)
+        pic120 TEXT,-- 歌曲封面120px缩略图
+        albuminfo TEXT,-- 专辑描述
+        name TEXT,-- 歌曲名称
         FOREIGN KEY (custom_play_list_id) REFERENCES custom_play_list(id)
         )''');
   }
